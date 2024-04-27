3e0923ea9cd0e9c85e80d9c47e4ccbe2f77edc0d
KamgaBrayan <brayanarmel@gmail.com>
2024-04-27 17:56:13 +0100

    Python library with basic maths functions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='kkba_mathUtils',
     packages=find_packages(include=['kkba_mathUtils']),
-    version='0.1.0',
+    version='1.0.0',
     description='My basic mathematics Python library',
     author='Kuate Brayan',
     install_requires=[],
